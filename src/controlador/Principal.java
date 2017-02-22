8cc4b73c6e59762a92eda1c8d7670fdf66857c31
josemalm32 <josema.aa14@gmail.com>
2017-02-22 10:53:26 +0100

    Inicial

---
+++ src/controlador/Principal.java
@@ -0,0 +1,31 @@
+package controlador;
+
+import java.awt.EventQueue;
+
+import vista.TresNRayaUI;
+
+@SuppressWarnings("serial")
+public class Principal extends TresNRayaUI{
+
+public Principal(){
+	
+	
+}
+	
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//IMPORTANTE: Se crea un objeto de Principal no del UI
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+}
